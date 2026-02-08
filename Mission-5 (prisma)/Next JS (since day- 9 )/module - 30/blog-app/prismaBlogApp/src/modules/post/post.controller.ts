878c76e26e0c170c3881bd8acd0f7fdb904f4141
Iftekhar <princemahmood117@gmail.com>
2026-02-09 01:10:14 +0600

    next project created

---
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 30/blog-app/prismaBlogApp/src/modules/post/post.controller.ts
@@ -0,0 +1,225 @@
+import { NextFunction, Request,Response } from "express";
+import { postService } from "./post.service";
+import { PostStatus } from "../../../generated/prisma/enums";
+import paginationSortingHelper from "../../helpers/paginationSortingHelper";
+import { UserRole } from "../../middlewares/authMiddleware";
+
+
+const createPost = async(req:Request, res: Response, next:NextFunction) => {
+
+    try {
+        const user = req.user;
+        if(!user) {
+            return res.status(400).json({
+            error : "User not found",            
+            })
+        }
+
+        const result = await postService.createPost(req.body, user.id as string)
+
+        res.status(201).json(result)
+    } catch (err) {
+
+        // handling error from global error handler [next() will go to the app.ts er app.use(errorHandler) part if error found]
+        next(err)
+
+        // res.status(400).json({
+        //     error : "Post creation failed",
+        //     details : err
+        // })
+    }
+}
+
+
+
+
+
+
+const getAllPost = async(req:Request, res:Response) => {
+    try {
+
+        const {search} = req.query;  // req.query contains URL query (after the '?' sign in the url)
+
+        const searchString = typeof search === 'string' ? search : undefined
+
+        const tags = req.query.tags ? (req.query.tags as string).split(',') : []
+
+        // const isFeatured = req.query.isFeatured ? req.query.isFeatured === 'true' : undefined;
+
+        // better approch
+        const isFeatured = req.query.isFeatured 
+        ? req.query.isFeatured === 'true' ? true : req.query.isFeatured === 'false' ? false : undefined
+        : undefined
+
+        const status = req.query.status as PostStatus | undefined;
+
+
+        // PAGINATION
+        // const page = Number(req.query.page ?? 1);
+        // const limit = Number(req.query.limit ?? 10)
+
+        // const skip = (page - 1) * limit;
+
+
+        // const sortBy = req.query.sortBy as string | undefined;
+        // const sortOrder = req.query.sortOrder as string | undefined;
+
+
+        const {page, limit, skip, sortBy, sortOrder} = paginationSortingHelper(req.query)
+        
+
+        const allPosts = await postService.getAllPosts({search : searchString, tags, isFeatured, status, page, limit, skip, sortBy, sortOrder})
+  
+        res.status(200).json(allPosts)
+
+    } catch (err) {
+        res.status(400).json({
+            error : "Post fetching failed",
+            details : err
+        })
+    }
+}
+
+
+const getPostById = async (req : Request, res : Response) => {
+
+    try {
+
+        const {postId} = req.params;
+        console.log(postId);
+        
+        if(!postId) {
+            throw new Error('post id is required!')
+        }
+
+        const result = await postService.getPostById(postId)
+        
+        res.status(201).json(result)
+        
+    } catch (err) {
+        res.status(400).json({
+            error : "Post fetching failed by id",
+            details : err
+        })
+    }
+}
+
+
+
+
+
+
+
+
+const getUsersPosts = async(req:Request, res:Response) => {
+
+    try {
+    const user = req.user
+
+    if(!user) {
+        throw new Error('User not found for single post fetch!')
+    }
+
+    const result = await postService.getUsersPosts(user?.id)
+    res.status(200).json(result)
+    } 
+    catch (err) {
+        const erroMessage = (err instanceof Error) ? err.message : "error inside single users post fetching!"  
+        res.status(400).json({
+            error : " single user's Post fetching failed",
+            details : erroMessage
+        })
+    }
+}
+
+
+
+
+// update post
+const updatePost = async(req:Request, res:Response, next:NextFunction) => {
+
+   try {
+    const user = req.user;
+    const {postID} = req.params;
+
+    if(!user) {
+        throw new Error("User not found for update")
+    }
+
+    const isAdmin = user?.role === UserRole.ADMIN
+
+    const updateData = req.body
+
+    const result = await postService.updatePost(postID as string, updateData, user?.id, isAdmin)
+    res.status(200).json(result)
+   }  
+   
+   catch (err) {
+        // const erroMessage = (err instanceof Error) ? err.message : "error inside single users post update!"  
+        // res.status(400).json({
+        //     error : "single user's Post update failed",
+        //     details : erroMessage
+        // })
+        next(err)
+    }
+}
+
+
+// statistics post
+const deletePost = async(req:Request, res:Response) => {
+
+    try {
+    
+    const user = req?.user;
+    const isAdmin = user?.role === UserRole.ADMIN
+    const {postID} = req.params;
+    
+    if(!user) {
+        throw new Error("User not found for update")
+    }
+
+    await postService.deletePost(postID as string, user.id, isAdmin)
+    res.status(200).json({
+        success : true,
+        message : `post id ${postID} delete successfull! `
+    })
+}   
+   catch (err) {
+        const erroMessage = (err instanceof Error) ? err.message : "error inside single users post update!"  
+        res.status(400).json({
+            error : "Failed to delete user's post!",
+            details : erroMessage
+        })
+    }
+}
+
+
+// Delete post
+const getStats = async(req:Request, res:Response) => {
+
+    try {
+
+    const result = await postService.getStats()
+    res.status(200).json(result)
+}   
+   catch (err) {
+        const erroMessage = (err instanceof Error) ? err.message : "error inside single users post update!"  
+        res.status(400).json({
+            error : "",
+            details : erroMessage
+        })
+    }
+}
+
+
+
+export const postController = {
+    createPost,
+    getAllPost,
+    getPostById,
+    getUsersPosts,
+    updatePost,
+    deletePost,
+    getStats
+}
+
---
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 30/blog-app/prismaBlogApp/src/modules/post/post.router.ts
@@ -0,0 +1,37 @@
+import express, { Router } from "express";
+import { postController } from "./post.controller";
+import authHandler, { UserRole } from "../../middlewares/authMiddleware";
+
+
+const router = express.Router();
+
+
+
+router.get('/', postController.getAllPost)
+
+
+router.get('/stats',authHandler(UserRole.ADMIN), postController.getStats)
+
+
+
+
+router.get('/my-posts', authHandler(UserRole.USER, UserRole.ADMIN), postController.getUsersPosts)
+
+
+router.get('/:postId', postController.getPostById)
+
+
+router.patch('/:postID', authHandler(UserRole.ADMIN, UserRole.USER), postController.updatePost)
+
+
+router.delete('/:postID', authHandler(UserRole.ADMIN, UserRole.USER), postController.deletePost)
+
+
+// Example route for creating a post
+router.post('/', authHandler(UserRole.ADMIN, UserRole.USER), postController.createPost)
+
+
+
+
+
+export const postRouter:Router = router;
