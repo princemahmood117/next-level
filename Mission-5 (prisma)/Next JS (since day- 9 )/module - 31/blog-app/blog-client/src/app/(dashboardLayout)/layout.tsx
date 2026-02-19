654c3bfb34e6f2efd5a7bd50e0d2a8836025fa0a
Iftekhar <princemahmood117@gmail.com>
2026-02-19 21:59:17 +0600

    types added to module 31

--- Mission-5 (prisma)/Next JS (since day- 9 )/module - 31/blog-app/blog-client/src/app/(dashboardLayout)/layout.tsx
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 31/blog-app/blog-client/src/app/(dashboardLayout)/layout.tsx
@@ -26,7 +26,7 @@ export default function DashboardLayout({admin, user} : {
 
   return (
     <SidebarProvider>
-      <AppSidebar />
+      <AppSidebar user = {userInfo}/>
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
           <SidebarTrigger className="-ml-1" />
--- Mission-5 (prisma)/Next JS (since day- 9 )/module - 31/blog-app/blog-client/src/components/app-sidebar.tsx
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 31/blog-app/blog-client/src/components/app-sidebar.tsx
@@ -12,49 +12,65 @@ import {
   SidebarRail,
 } from "@/components/ui/sidebar"
 import Link from "next/link"
+import { adminRoutes } from "@/routes/adminRoutes"
+import { userRoutes } from "@/routes/userRoutes"
+import { Route } from "@/types"
 
 // This is sample data.
-export const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Build Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Admin dashboard",
-          url: "/admin-dashboard",
-        },
-        {
-          title: "User Dashboard",
-          url: "/dashboard",
-        },
-      ],
-    },
+// export const data = {
+//   versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
+//   navMain: [
+//     {
+//       title: "Getting Started",
+//       items: [
+//         {
+//           title: "Installation",
+//           url: "nextjs.org",
+//         },
+//       ],
+//     },
+//     {
+//       title: "Build Your Application",
+//       items: [
+//         {
+//           title: "Admin dashboard",
+//           url: "/admin-dashboard",
+//         },
+//         {
+//           title: "User Dashboard",
+//           url: "/dashboard",
+//         },
+//       ],
+//     },
+//   ],
+// }
 
 
-  ],
-}
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+
+
+export function AppSidebar({user, ...props }: { user : {role : string} & React.ComponentProps<typeof Sidebar>}) {
+ 
+ let routes : Route[] = []
+
+  switch (user.role) {
+    case "admin" :
+      routes = adminRoutes;
+      break;
+
+    case "user" :
+      routes = userRoutes;
+      break;
+  
+    default:
+      routes = []
+      break;
+  }
+ 
   return (
     <Sidebar {...props}>
       <SidebarContent>
-        {data.navMain.map((item) => (
+        {routes.map((item) => (
           <SidebarGroup key={item.title}>
             <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
             <SidebarGroupContent>
