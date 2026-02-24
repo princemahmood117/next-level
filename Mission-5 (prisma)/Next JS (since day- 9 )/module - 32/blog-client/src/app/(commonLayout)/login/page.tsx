4201d471acc9747bd4fff858db245c2749aefc82
Iftekhar <princemahmood117@gmail.com>
2026-02-24 23:53:24 +0600

    login signup component atted

--- Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/app/(commonLayout)/login/page.tsx
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/app/(commonLayout)/login/page.tsx
@@ -1,10 +1,24 @@
 
-const LoginPage = () => {
-    return (
-        <div>
-            this is login page
-        </div>
-    );
-};
+// const LoginPage = () => {
+//     return (
+//         <div>
+//             this is login page
+//         </div>
+//     );
+// };
 
-export default LoginPage;
+// export default LoginPage;
+
+
+
+import { LoginForm } from "@/components/login-form"
+
+export default function LoginPage() {
+  return (
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
+}
