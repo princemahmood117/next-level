c5cd65583fa84fa894355217090b800c223152ae
Iftekhar <princemahmood117@gmail.com>
2026-02-25 05:14:51 +0600

    google auth implementing

--- Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/app/(commonLayout)/page.tsx
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/app/(commonLayout)/page.tsx
@@ -1,5 +1,11 @@
+import { authClient } from "@/lib/auth-client";
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await authClient.getSession()
+  console.log("session from auth-client", session);
+
+  
   return (
     <div></div>
   );
