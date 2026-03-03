d2fe371fede6c1fab6ca6af9e7d4cb9a3fd29962
Iftekhar <princemahmood117@gmail.com>
2026-03-04 01:11:41 +0600

    authentication using proxy and redirect

--- Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/proxy.ts
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/proxy.ts
@@ -1,8 +1,42 @@
 import { NextRequest, NextResponse } from "next/server"
+import { userService } from "./service/user.service";
+import { Roles } from "./constants/roles";
 
-export const proxy = (request : NextRequest) => {
+export const proxy = async (request : NextRequest) => {
 
-    console.log("request url from proxy : ", request.url);
+    const pathname = request.nextUrl.pathname
+
+    let isAuthenticated = false
+    let isAdmin = false;
+
+    const {data} = await userService.getSession()
+    console.log("data from proxy ", data);
+
+    // if session exists
+    if(data) {
+        isAuthenticated = true;
+        isAdmin = data.user.role === Roles.user
+    }
+
+    // if not authenticated / no session found
+    if(!isAuthenticated) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+
+    // If the user is an admin AND they are trying to access '/dashboard',
+    //    then redirect them to /admin-dashboard.
+    if(isAdmin && pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/admin-dashboard', request.url))
+    }
+
+
+    // If the user is not admin AND they are trying to access '/admin-dashboard',
+    //    then redirect them to /dashboard.
+    if(!isAdmin && pathname.startsWith('/admin-dashboard')) {
+        return NextResponse.redirect(new URL('/dashboard', request.url))
+    }
+    
 
     return NextResponse.next()
 
@@ -10,5 +44,9 @@ export const proxy = (request : NextRequest) => {
 
 
 export const config = {
-    matcher : ['/dashboard'],
+    matcher : [
+        '/dashboard', 
+        '/admin-dashboard', 
+        '/dashboard/:path*', 
+        '/admin-dashboard/:path*'],
 }
