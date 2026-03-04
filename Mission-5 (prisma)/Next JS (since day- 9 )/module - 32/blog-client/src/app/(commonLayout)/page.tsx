c72cb42f17f06ec2b556d6e312c74fb596067d54
Iftekhar <princemahmood117@gmail.com>
2026-03-05 01:39:18 +0600

    data fetch using next js successful

--- Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/app/(commonLayout)/page.tsx
+++ Mission-5 (prisma)/Next JS (since day- 9 )/module - 32/blog-client/src/app/(commonLayout)/page.tsx
@@ -1,34 +1,14 @@
 
 
-// import { cookies } from "next/headers";
+import { blogService } from "@/service/blog.service";
 
-import { userService } from "@/service/user.service";
 
 export default async function Home() {
 
-  const {data} = await userService.getSession()
+  const {data} = await blogService.getBlogsPost()
 
-  console.log('session from the service', data);
+  console.log(data);
 
-
-
-  
-
-  // // get cookie in the server component
-  // const cookieStore = await cookies();
-
-  // console.log('cookie store : ', cookieStore.toString());
-
-  // const res = await fetch("http://localhost:5000/api/auth/get-session", {
-  //   headers : {
-  //     Cookie : cookieStore.toString()
-  //   },
-  //   cache : 'no-cache',
-  // })
-  
-  // const session = await res.json()
-
-  // console.log('this is the session : ', session);
 
   
   return (
