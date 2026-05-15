315a8abfd57ee94eb10a5a6bb632836641fe4478
iftekhar <princemahmood117@gmail.com>
2026-05-15 23:49:34 +0600

    dash lay

--- Conceptual Session/Nextjs/Session-1/frontend/app/DashboardLayout/layout.tsx
+++ Conceptual Session/Nextjs/Session-1/frontend/app/DashboardLayout/layout.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+
 // this is dashboard layout where the children are rendered
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       {children}
+      this is layout chindreen
     </div>
   );
 };
