2f78a9e339819fd9c2ab826ee8450571f4e2ee06
iftekhar <princemahmood117@gmail.com>
2026-05-25 23:13:27 +0600

    'js

--- Conceptual Session/Nextjs/Session-1/frontend/app/(DashboardLayout)/layout.tsx
+++ Conceptual Session/Nextjs/Session-1/frontend/app/(DashboardLayout)/layout.tsx
@@ -1,16 +1,85 @@
-import React from "react";
+// import React from "react";
 
-// this is dashboard layout where the children are rendered
-const DashboardLayout = ({ admin, user }: { admin: React.ReactNode, user: React.ReactNode, }) => {
-  return (
-    <div>
+// // this is dashboard layout where the children are rendered parallely
+// const DashboardLayout = ({ admin, user }: { admin: React.ReactNode, user: React.ReactNode, }) => {
+//   return (
+//     <div>
 
-      {admin}
-      <br />
-      {user}
+//       {admin}
+//       <br />
+//       {user}
   
-    </div>
-  );
-};
+//     </div>
+//   );
+// };
 
-export default DashboardLayout;
+// export default DashboardLayout;
+
+
+
+
+import { AppSidebar } from "@/components/shared/sidebar/app-sidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { getCurrentUser } from "@/services/auth/auth";
+
+export default async function DashboardLayout({
+  admin,
+  user,
+}: {
+  admin: React.ReactNode;
+  user: React.ReactNode;
+}) {
+  const userRole = await getCurrentUser();
+
+  return (
+    <SidebarProvider>
+      <AppSidebar userRole={""} />
+      <SidebarInset>
+        <header className="sticky top-0 z-10 bg-background flex h-16 items-center justify-between px-4 border-b">
+          <div className="flex items-center gap-2">
+            <SidebarTrigger className="-ml-1" />
+            <Breadcrumb className="hidden sm:flex">
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink href="/dashboard">Home</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator />
+                <BreadcrumbItem>
+                  {userRole === "user" ? "User" : "Admin"}
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+
+        <main
+          className={`p-4 relative  pt-6 min-h-[calc(100vh-4rem)] gradientBg`}>
+          {user}
+
+          {admin}
+        </main>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
+
+
+
+
+
+
+
+
+
+
